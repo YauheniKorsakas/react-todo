4ffe373960fe79ec7ad941e253386f943ffe2bec
YauheniKorsakas <yauheni.korsakas@gmail.com>
2023-05-13 17:33:20 +0300

    Add TodoFooter. Add Button control. Add styling

--- src/components/ui/atoms/index.js
+++ src/components/ui/atoms/index.js
@@ -1,4 +1,5 @@
 import Checkbox from "./Checkbox/Checkbox";
 import TextInput from "./TextInput/TextInput";
+import Button from './Button/Button';
 
-export { Checkbox, TextInput };
+export { Checkbox, TextInput, Button };
